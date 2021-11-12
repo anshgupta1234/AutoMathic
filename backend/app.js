7c8756df227ea4b9475ffd936c757e68f65e83f6
Nathan R. Liu <nathanliu17@gmail.com>
2021-11-12 01:27:11 -0800

    accidentally committed with conflict markers

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -10,19 +9,4 @@ app.use('/test',test);
 
 app.listen(3000);
 console.log("Listening on Port 3000!");
-=======
-const app = require("express")();
-const PORT = 8080;
 
-app.listen(PORT, () => console.log("Api Server listening on port:" + PORT));
-
-/*
- * Sample get request handler
- *app.get(endpoint, (request, response) => {
- *  request.status(200).send({
- *    // JSON DATA
- *  }
- *  )
- *});
- */
->>>>>>> a4f31a8cd7c2fc79fae7d89b600128c38d5e2b7b
