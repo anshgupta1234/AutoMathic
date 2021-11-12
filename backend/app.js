4bfa2238d06865e02c92f29ea2c58422853f1d81
Siddharth Mohanty <neosiddharth@gmail.com>
2021-11-11 17:35:50 -0800

    Sample get-request handler(comment)

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
-const app = require('express')();
+const app = require("express")();
 const PORT = 8080;
 
-app.listen(
-  PORT,
-  () => console.log("Api Server listening on port:" + PORT)
-)
+app.listen(PORT, () => console.log("Api Server listening on port:" + PORT));
+
+/*
+ * Sample get request handler
+ *app.get(endpoint, (request, response) => {
+ *  request.status(200).send({
+ *    // JSON DATA
+ *  }
+ *  )
+ *});
+ */
