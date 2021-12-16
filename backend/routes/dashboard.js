0f431c707f01e6cbe32607369a818d7e47720b34
Siddharth Mohanty <neosiddharth@gmail.com>
2021-12-15 19:20:50 -0800

    Sort return by accessed, send status

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,19 +1,28 @@
 const express = require("express");
+
 const router = express.Router();
 const monglue = require("../app");
 const session = require("express-session");
+
+function sortByKey(array) {
+  // This works
+  return array.sort(function (a, b) {
+    var x = a["accessed"];
+    var y = b["accessed"];
+    return x < y ? -1 : x > y ? 1 : 0;
+  });
+}
 
 router.get('/', async function (req, res) {
   console.log("request received");
   if (req.session.username) {
     // TODO find from collection then sort
     const arr = await monglue.Test.find({});
-    res.send(arr);
+    res.status(200).send(sortByKey(arr));
   } else {
     // TODO error handling
-    res.send({ error: "Not logged in" });
+    res.status(401).send({ error: "Not logged in" });
   }
 });
-
 
 module.exports = router;
