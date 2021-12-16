96fad7f748e5078b97042fd3821feff1d545e712
Chinmay Devadhar <cdevadhar@gmail.com>
2021-12-15 19:04:31 -0800

    Updated /dashboard to simple working version

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const monglue = require("../app");
 const session = require("express-session");
 
-router.get("/", function (req, res, next) {
-  if (req.session.user) {
+router.get('/', async function (req, res) {
+  console.log("request received");
+  if (req.session.username) {
     // TODO find from collection then sort
+    const arr = await monglue.Test.find({});
+    res.send(arr);
   } else {
     // TODO error handling
+    res.send({ error: "Not logged in" });
   }
 });
+
 
 module.exports = router;
