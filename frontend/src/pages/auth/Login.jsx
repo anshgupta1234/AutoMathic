e88e02c9b310dee22d451325676a80c893fb6aba
toasty-alexus <93799880+toasty-alexus@users.noreply.github.com>
2021-12-19 13:38:19 -0800

    Update Login.jsx

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import "./login.css"
+import "login.css"
 export default class Login extends Component {
     render() { 
         return <div id='container'> 
