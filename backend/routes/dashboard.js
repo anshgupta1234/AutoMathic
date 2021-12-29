a7ad8dafe8d7a7204744a757a3ee08f0f2440740
Siddharth Mohanty <neosiddharth@gmail.com>
2021-12-28 16:51:56 -0800

    Add comments

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -5,7 +5,7 @@ const monglue = require("../app");
 const session = require("express-session");
 
 function sortByKey(array) {
-  // This works
+  // sort by "accessed"
   return array.sort(function (a, b) {
     var x = a["accessed"];
     var y = b["accessed"];
@@ -13,12 +13,15 @@ function sortByKey(array) {
   });
 }
 
-router.get('/', async function (req, res) {
+router.get("/", async function (req, res) {
   console.log("request received");
   if (req.session.username) {
+    // Access tests
     const arr = await monglue.Test.find({});
+    // Send sorted tests w/ code 200
     res.status(200).send(sortByKey(arr));
   } else {
+    // If user is not logged in, send code 401 w/ message: "Not logged in"
     res.status(401).send({ error: "Not logged in" });
   }
 });
