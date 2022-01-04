c36dcb550dd6aa6988ee7400d4ca50243910f79d
PIXEL SONIC <79956191+Quantum-Manu@users.noreply.github.com>
2022-01-04 15:47:00 -0800

    Update deleteTest.js

--- backend/routes/deleteTest.js
+++ backend/routes/deleteTest.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
 const monglue = require('../app');
 
 router.post('/', async function (req, res) {
@@ -17,9 +15,10 @@ router.post('/', async function (req, res) {
   // Finding the test with that id to delete
   const dTest = await monglue.Test.findOne( { testID: id } );
 
-  // Delete Test
+  // Delete Test with given id: dTest
   await monglue.Test.deleteOne( { testID: dTest } );
 
+  // Success message
   res.send({"success": `test with id:${id}, has been deleted`})
 
 });
